1c0d3031df912f14b83d55b0f28029e8460e8563
NandanaKrishna03 <nandanakrishna75@gmail.com>
2025-01-07 11:58:33 +0530

    Update server.js

--- todo-backend/server.js
+++ todo-backend/server.js
@@ -78,4 +78,7 @@ app.delete("/task/:id", (req, res) => {
 });
     
     
-app.listen(3007)
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
